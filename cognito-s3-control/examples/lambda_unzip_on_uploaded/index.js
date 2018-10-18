1d0e90e4ec358cb57432363a6dcfaa9651861837
Jason Guan Shuo Mai <trashmai@gmail.com>
2018-10-18 16:09:40 +0800

    generate and uplaod image metadata

--- cognito-s3-control/examples/lambda_unzip_on_uploaded/index.js
+++ cognito-s3-control/examples/lambda_unzip_on_uploaded/index.js
@@ -48,9 +48,13 @@ exports.handler = (event, context, callback) => {
 
     let parser = unzip.Parse({ decodeString: (buffer) => { return iconvLite.decode(buffer, 'utf8'); } });
 
-    let upsert_querys = [];
-    let relative_url_json = "camera-trap/json/" + upload_session_id + "/" + file_key_name_part + ".json";
-    let unzip_end = false;
+    let mma_upsert_querys = [];
+    let mma_relative_url_json = "camera-trap/json/" + upload_session_id + "/" + file_key_name_part + ".mma.json";
+
+    let mmm_upsert_querys = [];
+    let mmm_relative_url_json = "camera-trap/json/" + upload_session_id + "/" + file_key_name_part + ".mmm.json";
+    
+    let unzip_close = false;
     let cnt_of_exif_extracting = 0;
 
     s3.getObject(params).createReadStream()
@@ -86,6 +90,9 @@ exports.handler = (event, context, callback) => {
               else {
                 console.log(exifData); // Do something with your data!
 
+                // 太大了，暫時不存
+                delete exifData.MakerNote;
+
                 let dateTimeComponents = exifData.exif.DateTimeOriginal.match(/\d+/g);
                 let dateTimeString = dateTimeComponents[0] + "-" + dateTimeComponents[1] + "-" + dateTimeComponents[2] + " " + dateTimeComponents[3] + ":" + dateTimeComponents[4] + ":" + dateTimeComponents[5];
                 let timestamp = new Date(dateTimeString).getTime() / 1000;
@@ -99,34 +106,73 @@ exports.handler = (event, context, callback) => {
 
                 let full_location = tag_data.project + "/" + tag_data.site + "/" + tag_data.sub_site + "/" + tag_data.location;
                 let relocate_path = "camera-trap/images/orig/" + full_location;
+                let relocate_path_low_quality = "camera-trap/images/_res_quality_/" + full_location;
 
                 let relative_url = relocate_path + '/' + baseFileName + ".jpg";
                   
                 let _id = md5(relative_url);
                 let full_location_md5 = md5(full_location);
 
-                let upsert_query = {
+                let mma_upsert_query = {
                   _id: _id,
                   project: tag_data.project,
                   full_location_md5: full_location_md5,
+                  $set: { // 只能由多媒體檔案中擷取出的資訊，放在 $set。目的是補充先上傳 CSV 再上傳 多媒體檔時欠缺的 metadata
+                    modified_by: tag_data.user_id,
+                    type: "StillImage",
+                    date_time_original: exifData.exif.DateTimeOriginal,
+                  },
                   $setOnInsert: {
                     url: relative_url,
                     url_md5: _id,
-                    date_time_original: exifData.exif.DateTimeOriginal,
-                    date_time_original_timestamp: timestamp,
+                    date_time_original_timestamp: timestamp, // 這個值可從 CSV 中的拍照時間還原。或在相機設定錯誤時覆蓋掉 metadata
                     project: tag_data.project,
                     site: tag_data.site,
                     sub_site: tag_data.sub_site,
                     location: tag_data.location,
                     full_location_md5: full_location_md5,
                     timezone: "+8",
-                    tokens:[{}]
+                    tokens:[{
+                      data :[{
+                        key: "vernacular-name-zhtw",
+                        value: ""
+                      }]
+                    }]
                   },
                   $addToSet: {related_upload_sessions: upload_session_id},
                   $upsert: true
                 };
+                mma_upsert_querys.push(mma_upsert_query);
 
-                upsert_querys.push(upsert_query);
+                let mmm_upsert_query = {
+                  _id: _id,
+                  project: tag_data.project,
+                  full_location_md5: full_location_md5,
+                  $set: {
+                    modified_by: tag_data.user_id,
+                    type: "StillImage",
+                    date_time_original: exifData.exif.DateTimeOriginal,
+                    device_metadata: exifData.image,
+                    make: exifData.image.Make,
+                    model: exifData.image.Model,
+                    modify_date: exifData.image.ModifyDate,
+                    exif: exifData.exif
+                  },
+                  $setOnInsert: {
+                    url: relative_url,
+                    url_md5: _id,
+                    date_time_original_timestamp: timestamp,
+                    project: tag_data.project,
+                    site: tag_data.site,
+                    sub_site: tag_data.sub_site,
+                    location: tag_data.location,
+                    full_location_md5: full_location_md5,
+                    timezone: "+8"
+                  },
+                  $upsert: true
+                };
+                mmm_upsert_querys.push(mmm_upsert_query);
+
 
                 // original file upload
                 if (file_size)
@@ -141,24 +187,39 @@ exports.handler = (event, context, callback) => {
 
               } // end of exif extraction
               cnt_of_exif_extracting--;
-              if (cnt_of_exif_extracting == 0 && unzip_end) {
-                let upsert_querys_string = JSON.stringify(upsert_querys, null, 2);
-                console.log(relative_url_json);
-                s3.upload({Bucket: bucket, Key: relative_url_json, Body: upsert_querys_string, ContentType: "application/json"}, {},
+              if (cnt_of_exif_extracting == 0 && unzip_close) {
+                let mma_op = {
+                  endpoint:"/multimedia-annotations/bulk-update",
+                  post: mma_upsert_querys
+                }
+                let mma_upsert_querys_string = JSON.stringify(mma_op, null, 2);
+                console.log(mma_relative_url_json);
+                s3.upload({Bucket: bucket, Key: mma_relative_url_json, Body: mma_upsert_querys_string, ContentType: "application/json"}, {},
                   function(err, data) {
                     if (err) 
                       console.log('ERROR!');
                     else
                       console.log('OK');
                   });
-                // console.log(JSON.stringify(upsert_querys, null, 2));
+                // console.log(JSON.stringify(mma_upsert_querys, null, 2));
+                // -------------
+                let mmm_op = {
+                  endpoint:"/multimedia-metadata/bulk-update",
+                  post: mmm_upsert_querys
+                }
+                let mmm_upsert_querys_string = JSON.stringify(mmm_op, null, 2);
+                console.log(mmm_relative_url_json);
+                s3.upload({Bucket: bucket, Key: mmm_relative_url_json, Body: mmm_upsert_querys_string, ContentType: "application/json"}, {},
+                  function(err, data) {
+                    if (err) 
+                      console.log('ERROR!');
+                    else
+                      console.log('OK');
+                  });
               }
-
             });
 
-            // 暫時不處理縮圖
             // diff resolutions and quality settings
-            if (0) // turn off testing
             for (let res_idx = 4; res_idx <= 8; res_idx++) {
               for (let quality = 60; quality <= 80; quality = quality + 10) {
                 let width = 128 * res_idx;
@@ -173,8 +234,9 @@ exports.handler = (event, context, callback) => {
                   .toBuffer()
                   .then(function(resized_data){
                     // 再用EXIF做為重新命名的依據
+                    console.log(relocate_path_low_quality);
                     if (file_size)
-                    s3.upload({Bucket: bucket, Key: 'public_images/' + width + 'q' + quality + '/' + fileName + ".webp", Body: resized_data, ContentType: "image/webp"}, {},
+                    s3.upload({Bucket: bucket, Key: relocate_path_low_quality.replace("_res_quality_", width + "q" + quality) + "/" + fileName + ".webp", Body: resized_data, ContentType: "image/webp"}, {},
                       function(err, data) {
                         if (err) 
                           console.log('ERROR!');
@@ -183,22 +245,25 @@ exports.handler = (event, context, callback) => {
                       });
 
                   });
-                }
+                break; // 最低畫質
+              }
+              break; // 最低解析度
             }
           });
         }
       }) // end of on entry
-      .on('finish', function() {
-        console.log("***************** UNZIP FINISH *****************");
-      })
-      .on('end', function() {
-        unzip_end = true;
-        console.log("***************** UNZIP END *****************");
-      })
-      .on('done', function() {
-        console.log("***************** UNZIP DONE *****************");
-      })
+      //.on('finish', function() {
+      //  console.log("***************** UNZIP FINISH *****************");
+      //})
+      //.on('end', function() {
+      //  console.log("***************** UNZIP END *****************");
+      //})
+      //.on('done', function() {
+      //  console.log("***************** UNZIP DONE *****************");
+      //})
       .on('close', function() {
+        // unzip-stream 官方推薦 close event
+        unzip_close = true;
         console.log("***************** UNZIP CLOSE *****************");
       }); 
   });
